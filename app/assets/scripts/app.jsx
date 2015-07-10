cafebf73480e33b43d5d1c7cc74df1bfd17bd0eb
Lucas <lucas@agtlucas.com>
2015-07-10 20:53:59 -0300

    Update app.jsx

---
+++ app/assets/scripts/app.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Router, Route, RouteHandler, Link, DefaultRoute, NotFoundRoute} from 'react-router';
+import Home from './home';
+import About from './about';
+
+/**
+ * App Class component
+ * @class
+ *
+ * @return {Component}
+ **/
+class App extends React.Component {
+
+  /**
+   * Render method
+   * @method
+   *
+   * @return {Component}
+   **/
+  render() {
+    return (
+      <section className="o-wrapper">
+        <RouteHandler />
+      </section>
+    );
+  }
+
+};
+
+/**
+ * Create route component
+ * @method
+ *
+ **/
+var routes = (
+  <Route handler={App}>
+    <DefaultRoute name="home" handler={Home} />
+    <Route name="about" path="/about" handler={About} />
+  </Route>
+);
+
+/**
+ * Initialize route component using React Router
+ * @method
+ *
+ * @return {Component}
+ **/
+Router.run(routes, Router.HistoryLocation, (Root) => {
+  React.render(<Root />, document.getElementById('js-main-root'));
+});
