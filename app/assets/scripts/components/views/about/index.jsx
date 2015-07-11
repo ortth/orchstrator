161b663f1dd75df8bde879c8e8c7f65a9150175a
Lucas <lucas@agtlucas.com>
2015-07-11 13:49:38 -0300

    Adding documentation to about view

--- app/assets/scripts/components/views/about/index.jsx
+++ app/assets/scripts/components/views/about/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+/**
+ * About Class component
+ * @class
+ *
+ * @return {Component}
+ **/
 export class About extends React.Component {
 
+  /**
+   * Render method
+   * @method
+   *
+   * @return {Component}
+   **/
   render() {
     return (
       <div className="o-content c-about-content">
