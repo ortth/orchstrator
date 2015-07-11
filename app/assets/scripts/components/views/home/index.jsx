557ddb9bcfdc1ac52c2ebd8d57436a1c7f92a9a5
Lucas <lucas@agtlucas.com>
2015-07-11 13:52:15 -0300

    Adding documentation to home view

--- app/assets/scripts/components/views/home/index.jsx
+++ app/assets/scripts/components/views/home/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router';
 
+/**
+ * Home Class component
+ * @class
+ *
+ * @return {Component}
+ **/
 export class Home extends React.Component {
 
+  /**
+   * Render method
+   * @method
+   *
+   * @return {Component}
+   **/
   render() {
     return (
       <div className="o-content c-home-content">
